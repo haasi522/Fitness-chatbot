c7785ae92906f17a83eff136a7d6a38247acb932
haasi522 <haasika.01@gmail.com>
2025-03-19 11:12:56 +0530

    Update fitness.py

--- fitness.py
+++ fitness.py
@@ -5,7 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-st.set_page_config(page_title="Smart Fitness Chatbot 💪", page_icon="💪", layout="centered"")
+st.set_page_config(page_title="Smart Fitness Chatbot 💪", page_icon="💪", layout="centered")
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
